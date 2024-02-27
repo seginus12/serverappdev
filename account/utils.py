bccafbdf5ae379da5bc2353d15aa43e28ddcd08d
Dmitry Shcherbakov <volchara@huawei.auf>
2024-02-27 23:54:25 +0800

    get valid tokens fun

--- account/utils.py
+++ account/utils.py
@@ -3,6 +3,8 @@ import string
 from django.core.mail import send_mail
 from django.conf import settings
 from .models import CustomUser, OneTimePassword
+from rest_framework_simplejwt.token_blacklist.models import OutstandingToken
+import datetime
 
 
 def generate_otp(length=settings.OTP_LENGTH):
@@ -18,3 +20,11 @@ def send_otp_email(email, otp):
     # user = CustomUser.objects.get(email=email)
     # OneTimePassword.objects.create(user=user, code=otp)
     send_mail(subject, message, from_email, recipient_list)
+
+def get_valid_refresh_count(user):
+    user_tokens = OutstandingToken.objects.filter(user=user)
+    vaild_tokens_count = 0
+    for token in user_tokens:
+        if (token.expires_at.replace(tzinfo=None) - datetime.datetime.now()) > 0:
+            vaild_tokens_count += 1
+    return vaild_tokens_count
